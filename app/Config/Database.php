1c57f70056503204378245d3755c3eec1c465acc
OmarBaltaji <omr.baltaji@gmail.com>
2021-11-14 23:29:00 +0200

    refactor: added support for sql server

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -23,16 +23,16 @@ class Database extends Config
      *
      * @var string
      */
-    public $defaultGroup = 'default';
+    public $defaultGroup = 'SqlServer';
 
     /**
-     * The default database connection.
+     * The mysql database connection.
      *
      * @var array
      */
-    public $default = [
+    public $MySQL = [
         'DSN'      => '',
-        'hostname' => 'localhost',
+        'hostname' => '',
         'username' => '',
         'password' => '',
         'database' => '',
@@ -51,29 +51,54 @@ class Database extends Config
     ];
 
     /**
+     * The SQL Server database connection.
+     *
+     * @var array
+     */
+    public $SqlServer = [
+		'DSN'      => 'sqlsrv:dbname=gallery_app;host=LAPTOP-7JAV6MP7',
+		'hostname' => 'LAPTOP-7JAV6MP7',
+		'username' => 'sa',
+		'password' => 'Bolto_23@yt',
+		'database' => 'gallery_app',
+		'DBDriver' => 'sqlsrv',
+		'DBPrefix' => '',
+		'pConnect' => false,
+		'DBDebug'  => (ENVIRONMENT !== 'development'),
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+		'swapPre'  => '',
+		'encrypt'  => false,
+		'compress' => false,
+		'strictOn' => false,
+		'failover' => [],
+		'port'     => 1433,        
+    ];
+
+    /**
      * This database connection is used when
      * running PHPUnit database tests.
      *
      * @var array
      */
     public $tests = [
-        'DSN'      => '',
-        'hostname' => '127.0.0.1',
-        'username' => '',
-        'password' => '',
-        'database' => ':memory:',
+ 		'DSN'      => '',
+		'hostname' => '127.0.0.1',
+		'username' => '',
+		'password' => '',
+		'database' => ':memory:',
         'DBDriver' => 'SQLite3',
         'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
         'pConnect' => false,
         'DBDebug'  => (ENVIRONMENT !== 'production'),
-        'charset'  => 'utf8',
-        'DBCollat' => 'utf8_general_ci',
-        'swapPre'  => '',
-        'encrypt'  => false,
-        'compress' => false,
-        'strictOn' => false,
-        'failover' => [],
-        'port'     => 3306,
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+		'swapPre'  => '',
+		'encrypt'  => false,
+		'compress' => false,
+		'strictOn' => false,
+		'failover' => [],
+		'port'     => 3306,        
     ];
 
     public function __construct()
